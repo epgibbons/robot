a3d9923efb7b3c30923d6eddbc6df9c8cec0c8f6
E P Gibbons <edwinpaulgibbons@gmail.com>
2016-09-06 12:16:13 +0100

    Make diagnostic statements dependent on #define DEBUG

--- sketches/Monitoring_Xbee_IO_packets/Monitoring_Xbee_IO_packets.ino
+++ sketches/Monitoring_Xbee_IO_packets/Monitoring_Xbee_IO_packets.ino
@@ -10,8 +10,44 @@
 
 #include <SoftwareSerial.h>
 
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
+ #define DEBUG_PRINTBIN(x)     Serial.print (x, BIN)
+ #define DEBUG_PRINTHEX(x)     Serial.print (x, HEX)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+ #define DEBUG_PRINTDECLN(x)  Serial.println (x,DEC)
+ #define DEBUG_PRINTBINLN(x)  Serial.println (x,BIN)
+ #define DEBUG_PRINTHEXLN(x)  Serial.println (x,HEX)
+#else
+ #define DEBUG_PRINT(x)   
+ #define DEBUG_PRINTDEC(x) 
+ #define DEBUG_PRINTBIN(x)     
+ #define DEBUG_PRINTHEX(x) 
+ #define DEBUG_PRINTLN(x) 
+ #define DEBUG_PRINTDECLN(x) 
+ #define DEBUG_PRINTBINLN(x) 
+ #define DEBUG_PRINTHEXLN(x)
+#endif
 
 SoftwareSerial mySerial(10, 11); // RX, TX
+
+struct dio{
+  byte enabled;
+  byte val;
+};
+struct aio{
+  byte enabled;
+  int val;
+};
+
+struct data{
+  byte valid;
+  struct dio dios[8];
+  struct aio aios[6];  
+};
+
+struct data getData();
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -34,61 +70,89 @@ void setup() {
 }
 
 void loop() { // run over and over
+  struct data d;
+  d = getData();
+  if( d.valid){
+    for( int i=0; i<8; i++){
+      if( d.dios[i].enabled)
+        Serial.println(d.dios[i].val);
+      else
+        DEBUG_PRINTLN("Not");
+      
+    }
+    for( int i=0; i<6; i++){
+      if( d.aios[i].enabled)
+        Serial.println(d.aios[i].val);
+      else
+        DEBUG_PRINTLN("Not");
+    }
+
+  }
+}
+
+struct data getData(){
+  struct data d ={ 0,{{0,0}}};
+  DEBUG_PRINTLN("getData");
   if (mySerial.available()) {
     byte start_delim=mySerial.read();
     if( start_delim == 0x7e){
-      Serial.println("Start (0x7E) found. ");
+      DEBUG_PRINTLN("Start (0x7E) found. ");
       byte len[2];
       if( mySerial.readBytes(len,sizeof(len))==sizeof(len)){
-        Serial.print("Length = ");
+        DEBUG_PRINT("Length = ");
         int length = (len[0]<<8) + len[1];
-        Serial.println(length);
+        DEBUG_PRINTLN(length);
         byte frameData[length];
 
         if( mySerial.readBytes(frameData,sizeof(frameData))==sizeof(frameData)){
           int j=0;
           byte api = frameData[j++];
 
-          Serial.print("API = ");
-          Serial.println(api,HEX);
+          DEBUG_PRINT("API = ");
+          DEBUG_PRINTHEXLN(api);
           if( api==0x82){
-            Serial.println("RX (Receive) Packet: 64-bit address I/O");
+            DEBUG_PRINTLN("RX (Receive) Packet: 64-bit address I/O");
             //0x82 for RX (Receive) Packet: 64-bit address I/O
-            Serial.print("Address = ");
+            DEBUG_PRINT("Address = ");
             for(int i= 0; i< 8; i++){
               byte ch = frameData[j++];
-              Serial.print(ch, HEX);
+              DEBUG_PRINTHEX(ch);
             }
-            Serial.println();
+            DEBUG_PRINTLN();
             byte rssi = frameData[j++];
-            Serial.print("RSSI = ");
-            Serial.println(rssi, HEX);
+            DEBUG_PRINT("RSSI = ");
+            DEBUG_PRINTHEXLN(rssi);
             byte options = frameData[j++];
-            Serial.print("Options = ");
-            Serial.println(options, HEX);
+            DEBUG_PRINT("Options = ");
+            DEBUG_PRINTHEXLN(options);
 
             byte numSamples = frameData[j++];
-            Serial.print("Number of samples  = ");
-            Serial.println(numSamples, HEX);
+            DEBUG_PRINT("Number of samples  = ");
+            DEBUG_PRINTHEXLN(numSamples);
 
             byte channelIndicator1 = frameData[j++];
-            Serial.print("Channel indicator 1 = ");
-            Serial.println(channelIndicator1  , BIN);
+            DEBUG_PRINT("Channel indicator 1 = ");
+            DEBUG_PRINTBINLN(channelIndicator1 );
             byte channelIndicator2 = frameData[j++];
-            Serial.print("Channel indicator 2 = ");
-            Serial.println(channelIndicator2, BIN);
+            DEBUG_PRINT("Channel indicator 2 = ");
+            DEBUG_PRINTBINLN(channelIndicator2);
             if( (channelIndicator1 & 0x1) || (channelIndicator2 != 0x0)){
               byte dio1 = frameData[j++];
               byte dio2 = frameData[j++];
               for( int dio=0; dio < 8; dio++){
-                Serial.print("DIO");
-                Serial.print(dio);
+
                 if( channelIndicator2 & (0x1<<dio) ){
+                  Serial.print("DIO");
+                  Serial.print(dio);                  
+                  d.dios[dio].enabled=1;
+                  d.dios[dio].val= (dio2 & 0x1<<dio) != 0;
                   Serial.print(" is enabled = ");
                   Serial.println( (dio2 & 0x1<<dio) != 0);
                 }
                 else {
-                  Serial.println(" is NOT enabled");
+                  DEBUG_PRINT("DIO");
+                  DEBUG_PRINT(dio);                  
+                  DEBUG_PRINTLN(" is NOT enabled");
                 }
                 
               }
@@ -97,23 +161,27 @@ void loop() { // run over and over
                 Serial.println( dio1 & 0x1);
               }
               else {
-                Serial.println("DIO8 is NOT enabled");
+                DEBUG_PRINTLN("DIO8 is NOT enabled");
 
               }
             }
             for(int adc=0; adc< 7; adc++){
-              Serial.print("ADC A");
-              Serial.print(adc);
               byte checkCode = 0x1<<(adc+1);
               if( channelIndicator1 & checkCode){
+                 Serial.print("ADC A");
+                 Serial.print(adc);
                  Serial.print(" is enabled  = ");
                  byte msb = frameData[j++];
                  byte lsb = frameData[j++];
                  int val = (msb<<8) + lsb;
+                 d.aios[adc].enabled=1;
+                 d.aios[adc].val= val;
                  Serial.println( val, DEC);
               }
               else {
-                 Serial.println(" is NOT enabled");
+                DEBUG_PRINT("ADC A");
+                DEBUG_PRINT(adc);
+                 DEBUG_PRINTLN(" is NOT enabled");
               }
             }
             byte check = mySerial.read();
@@ -122,14 +190,16 @@ void loop() { // run over and over
               verify += frameData[i];
             }
             if( check + verify != 0xFF){
-              Serial.println("Check sum is correct");
+              DEBUG_PRINTLN("Check sum is correct");
+              d.valid=1;
             }
             else {
-              Serial.println("Check sum is incorrect");
+              DEBUG_PRINTLN("Check sum is incorrect");
             }
          }
       }
     }
    }
   }
+  return d;
 }
